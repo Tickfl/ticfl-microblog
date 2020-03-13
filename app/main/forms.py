3c83e7c399c0d23c2bc3f1d4be3c9c7e6f73d347
Tickfl <49989307+Tickfl@users.noreply.github.com>
2020-03-13 06:42:07 +0500

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -25,3 +25,8 @@ class EditProfileForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField(('Say something'), validators=[DataRequired()])
     submit = SubmitField(('Submit'))
+
+class MessageForm(FlaskForm):
+    message = TextAreaField(('Message'), validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField(('Submit'))
